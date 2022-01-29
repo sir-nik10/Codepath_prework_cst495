f8d057e39ea7740108a0e16245d960b90410785d
Nick Flores <joseflores2@csumb.edu>
2022-01-28 17:28:02 -0800

    Added prework files to repository

---
+++ CST495_Prework_TipCalculatorUITests/CST495_Prework_TipCalculatorUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CST495_Prework_TipCalculatorUITestsLaunchTests.swift
+//  CST495_Prework_TipCalculatorUITests
+//
+//  Created by Nick Flores on 1/28/22.
+//
+
+import XCTest
+
+class CST495_Prework_TipCalculatorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
